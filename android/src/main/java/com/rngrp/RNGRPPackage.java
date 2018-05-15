8b19132249fd7ef06e97b194072b45b6a3b47bc8
Ben Hansen <bnhansn@gmail.com>
2018-05-14 18:42:15 -0700

    fix package override

--- android/src/main/java/com/rngrp/RNGRPPackage.java
+++ android/src/main/java/com/rngrp/RNGRPPackage.java
@@ -8,21 +8,21 @@ import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class RNGRPPackage implements ReactPackage {
+
+  @Override
+  public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+    return Collections.emptyList();
+  }
 
   @Override
   public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
     List<NativeModule> modules = new ArrayList<>();
     modules.add(new GRP(reactContext));
     return modules;
-  }
-
-  public List<Class<? extends JavaScriptModule>> createJSModules() {
-    return Collections.emptyList();
-  }
-
-  @Override
-  public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
-    return Arrays.<ViewManager>asList();
   }
 }
